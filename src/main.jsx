3c6eacc1f903767a52e51881ad481cd20bee2443
Kthostore <kthostore@gmail.com>
2025-12-02 06:50:20 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,18 @@
-
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from '@/App';
-import '@/styles/index.css';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import App from './App'
+import Admin from './admin/Admin'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
-);
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/admin" element={<Admin />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+)
+
